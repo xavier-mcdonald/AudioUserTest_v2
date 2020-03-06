6a956c2cff9ac0f9b77eb1279f435c8f553450d7
xavier-mcdonald <60202887+xavier-mcdonald@users.noreply.github.com>
2020-03-06 11:05:06 -0700

    working stuff

---
+++ ChangeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScript : MonoBehaviour {
+
+    public GameObject cube1, cube2;
+    bool happenedOnce = false;
+
+    private void Start()
+    {
+
+        DontDestroyOnLoad(cube1);
+        DontDestroyOnLoad(cube2);
+
+    }
+
+    void Update() {
+
+
+        // tells to stop loading scene so that it only happens once.
+        if (!happenedOnce && Input.GetKeyDown(KeyCode.Space))
+        {
+            happenedOnce = true;
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+            
+
+        }
+
+
+
+    }
+
+    
+}
+
